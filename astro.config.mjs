d810180b88893a40307e24bd911ee548d9652212
asus-fedora <davot098@gmail.com>
2025-02-11 19:19:32 -0500

    Update success. Loss animation in Header problem with @apply, refactor code and works.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
 
 import robotsTxt from "astro-robots-txt"
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  integrations: [robotsTxt()],
+  site: 'https://porfolio.dev/',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
