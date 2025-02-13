974a2223e790530654bcb2c1293bbf25ae9efc34
asus-fedora <davot098@gmail.com>
2025-02-13 01:15:41 -0500

    Fix adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config'
-
 import robotsTxt from "astro-robots-txt"
-
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [robotsTxt()],
   site: 'https://porfolio-dev-drab.vercel.app/',
   output: "server",
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   }
