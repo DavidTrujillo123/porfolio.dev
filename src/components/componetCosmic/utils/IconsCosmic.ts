d1cd35d46bae642a28450a23587e45ab2cf7d0c3
asus-fedora <davot098@gmail.com>
2025-02-14 00:42:41 -0500

    Add Java, JavaScript, and Laravel icons; update project tags

--- src/components/componetCosmic/utils/IconsCosmic.ts
+++ src/components/componetCosmic/utils/IconsCosmic.ts
@@ -1,4 +1,7 @@
 import AstroIcon from "@/components/icons/AstroIcon.astro";
+import Java from "@/components/icons/Java.astro";
+import JavaScript from "@/components/icons/JavaScript.astro";
+import Laravel from "@/components/icons/Laravel.astro";
 import LinkedInIcon from "@/components/icons/LinkedIn.astro";
 import MailIcon from "@/components/icons/Mail.astro";
 import NextJS from "@/components/icons/NextJS.astro";
@@ -30,5 +33,20 @@ export const TAGS: any = {
     name: "Storyblok",
     class: "bg-[#f8f8f8] text-black",
     icon: StoryBlok
+  },
+  java: {
+    name: "Java",
+    class: "bg-slate-300 text-black",
+    icon: Java
+  },
+  js: {
+    name: "JavaScript",
+    class: "bg-yellow-100 text-black",
+    icon: JavaScript
+  },
+  laravel: {
+    name: "Laravel",
+    class: "bg-gray-700 text-white",
+    icon: Laravel
   }
 };
